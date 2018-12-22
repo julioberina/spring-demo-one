d352b11d35763144c8f30b2fcadd7b8db0cc1faa
Julio Berina <julioberina@gmail.com>
2018-12-22 12:43:29 -0800

    Create BasketballCoach implementation of Coach

---
+++ src/com/luv2code/springdemo/BasketballCoach.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo;
+
+public class BasketballCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Shoot some hoops, especially 3's";
+	}
+
+}
